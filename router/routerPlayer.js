bcd7614254423a81ee530deef4ab049f4622c2f7
Israel Vitzman <iv058.l.v@gmail.com>
2025-07-18 16:23:34 +0300

    .

--- router/routerPlayer.js
+++ router/routerPlayer.js
@@ -4,11 +4,12 @@ import { AddResult } from "../controlessPlayer/addResultGame.js";
 const router = express.Router();
 
 export function RouterPlayer() {
+
     router.post('/dataPlayer', (req, res) => {
         AddResult(req, res)
     });
 
-    router.post('/cheakPlayer', (req, res) => {
+    router.post('/newPlayer', (req, res) => {
         NewPlayer(req, res)
 
     });
