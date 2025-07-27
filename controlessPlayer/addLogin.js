a9b4789fa99134d6550eb7a05964238431bacc17
Israel Vitzman <iv058.l.v@gmail.com>
2025-07-27 15:10:27 +0300

    ....

--- controlessPlayer/addLogin.js
+++ controlessPlayer/addLogin.js
@@ -6,7 +6,7 @@ const SECRET = 'your_jwt_secret_key';
 
 export async function LoginPlayer(req, res) {
     try {
-        const {name, role} = req.body;
+        const { name, role } = req.body;
 
         if (!name || !role) {
             return res.status(400).send("Missing name or role");
@@ -16,36 +16,36 @@ export async function LoginPlayer(req, res) {
 
         const query = `SELECT * FROM users WHERE name = ?`;
         const [rows] = await connection.execute(query, [name]);
-        
-        
+
+
         await connection.end();
 
         if (rows.length === 0) {
             return await NewPlayer(name, role, res);
         }
 
-       
-        console.log(user.role);
-        
+        const user = rows[0]
+
+
         const token = jwt.sign(
-            { 
+            {
                 id: user.id,
                 name: user.name,
-                role: user.role 
+                role: user.role
             },
             SECRET,
             { expiresIn: '1h' }
         );
 
-        
-        
+
+
 
         console.log(`login successful: ${user.name} (${user.role})`);
         console.log(`token sent in token: ${token.substring(0, 20)}...`);
-        
+
         console.log("created token for user");
-        res.status(200).json({token})
-            
+        res.status(200).json({ token })
+
 
     } catch (err) {
         console.error("Login error", err);
